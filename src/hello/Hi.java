5bd90332f1f9492f6520c7f4f8c266d3c85636bd
Izb123 <yvettegaju@gmail.com>
2022-08-10 12:52:16 -0500

    1st commit

---
+++ src/hello/Hi.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String msg = "Hello";
+		System.out.println(msg);
+	}
+
+}
